a83aa00fe83410568b64d1cd7f002406c8da5200
= <=>
2021-09-04 15:24:05 +0530

    updates auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,9 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 
 export const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
-	if (req.session.userId) next();
+	if (req.session.userId) return next();
 	res.status(401).json({
 		success: false,
 		message: "you need to be logged in to perform this action",
+	});
+};
+
+export const hasProjectOpened = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (req.session.projectId) return next();
+	res.status(401).json({
+		success: false,
+		message: "you need to have a open project to perform this action",
 	});
 };
